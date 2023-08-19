fdcf522ae496b788e7ebb3aabe5a0c960f8fa86a
TugaCode <manueljsb07@gmail.com>
2023-08-19 20:09:08 +0100

    Remover train.py

--- train.py
+++
@@ -1,7 +0,0 @@
-from TugaInput import TugaInput as tg
-
-tuga_input = tg(lang="en")
-
-entrada_traduzida = tuga_input.input_pt("Enter your feedback: ")
-
-print("Entrada traduzida para português:", entrada_traduzida)
